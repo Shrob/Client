ecbd4e100439c20664c4b47af80d9acafc5c777d
Shrob <achref.gharyeni@gmail.com>
2013-05-07 13:03:26 +0100

    first commit

---
+++ src/Client.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+/**
+ *
+ * @author Shrob
+ */
+public class Client {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws NotBoundException, MalformedURLException, RemoteException {
+        // TODO code application logic here
+        //try {
+calcul c=(calcul) Naming.lookup("rmi://localhost:1099/Mycalcul");
+double result1=c.carre(2);
+System.out.println(result1);
+//}
+//catch (Exception e) 
+//{System.out.println("Erreur client");
+//System.out.println(e.toString());}
+    }
+}
---
+++ src/calcul.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Shrob
+ */
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface calcul extends Remote {
+double carre(double x) throws RemoteException;
+double racine(double x) throws RemoteException;
+}
